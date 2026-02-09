a1f26c40f1741b9b2bd7e59a9014915d9290b195
Timofey <timazayworker@gmail.com>
2026-02-09 23:26:52 +0300

    init commit

---
+++ src/main/java/by/timazaytsev/messengerproject/MessengerProjectApplication.java
@@ -0,0 +1,13 @@
+package by.timazaytsev.messengerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerProjectApplication.class, args);
+	}
+
+}
