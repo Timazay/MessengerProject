2e58f0753ab12f3252dcfb1248eb4ce2ff5818d0
Timofey <timazayworker@gmail.com>
2026-03-03 16:54:06 +0300

    remove method from global exception handler

--- src/main/java/by/timazaytsev/messengerproject/features/authorization/common/GlobalExceptionHandler.java
+++ src/main/java/by/timazaytsev/messengerproject/features/authorization/common/GlobalExceptionHandler.java
@@ -1,24 +1,17 @@
 package by.timazaytsev.messengerproject.features.authorization.common;
 
 import by.timazaytsev.messengerproject.api.common.exception.ErrorResponseDto;
+import by.timazaytsev.messengerproject.api.common.exception.NotFoundException;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ConstraintViolationException;
-import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.time.DateTimeException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -30,12 +23,7 @@ public class GlobalExceptionHandler {
                     mediaType = MediaType.APPLICATION_JSON_VALUE,
                     schema = @Schema(implementation = ErrorResponseDto.class)))
     @ExceptionHandler({
-            ConstraintViolationException.class,
-            BadRequestException.class,
-            MissingServletRequestParameterException.class,
-            HttpMessageNotReadableException.class,
-            IllegalArgumentException.class,
-            DateTimeException.class
+            ConstraintViolationException.class
     })
     public ResponseEntity<ErrorResponseDto> handleBadRequests(Exception e) {
         return new ResponseEntity<>(
@@ -44,18 +32,9 @@ public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler({
-            EntityNotFoundException.class
+            NotFoundException.class
     })
     public ErrorResponseDto handleNotFoundException(Exception e) {
         return new ErrorResponseDto("404 NOT FOUND", e.getMessage());
-    }
-
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    @ExceptionHandler({
-            UsernameNotFoundException.class,
-            BadCredentialsException.class
-    })
-    public ErrorResponseDto handleBadRequestsException(Exception e) {
-        return new ErrorResponseDto("401 UNAUTHORIZED", e.getMessage());
     }
 }
